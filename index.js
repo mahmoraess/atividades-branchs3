612d7b018d0c8a59126a133f18e4976cbd2e889a
mahmoraes <maria.moraes6@estudante.ifms.edu.br>
2025-10-01 17:02:01 -0400

    iniciando função calcular

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-function calcularMedia(notas) {
-    let soma = 0;
-    for (let nota of notas) {
-        soma += nota;
-    }
-    return soma / notas.length;
-}
-
-function celsiusParaFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
 
 function saudacao(nome) {
     return "Olá" + nome; 
 }
 
-console.log("Média:", calcularMedia([10, 8, 6])); // esperado: 8
-console.log("Temperatura:", celsiusParaFahrenheit(25)); // esperado: 77
-console.log("Saudação:", saudacao("Maria")); // esperado: "Olá Maria"
+function calcularMedia(notas) {
+    let soma = 0;
+    for (let nota of notas) {
+        soma += nota;
+    }
+    return soma;
+}
